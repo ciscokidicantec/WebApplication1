6ec40366563c83317e617a784df6ad485c92a9bd
ciscokidicantec <cisco.kidicantec@googlemail.com>
2020-02-18 15:23:32 +0000

    Update WebForm1.aspx.cs

--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -19,7 +19,7 @@ namespace WebApplication1
         protected void Button1_Click(object sender, EventArgs e)
         {
             string lab;
-            lab = "hellow world 18-02-202021 22:00 version";
+            lab = "hellow world 18-02-202021 15:22 version";
             Label1.Text = lab;
         }
     }
